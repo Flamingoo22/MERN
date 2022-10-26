8894914dc67d4d2b4a9e93882bafdc306b0a0841
Flamingoo22 <110867987+Flamingoo22@users.noreply.github.com>
2022-10-25 23:15:39 -0400

    big inversion

---
+++ React/classComponent/cssInJS.js
@@ -0,0 +1,16 @@
+//npm install styled-components
+
+//src/components/StyledBox.js
+
+
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledBox = styled.div`
+    border: 1px solid lightgray;
+    background: ${props => props.bgColor};
+    width: ${props => props.width || '100px'};
+    height: ${props => props.height || '100px'};
+`;
+
+export default StyledBox;
