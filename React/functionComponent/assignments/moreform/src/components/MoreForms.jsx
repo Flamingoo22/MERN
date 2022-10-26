d297c446336aeb18b1bc9d9104cd2ed6e24d9487
Flamingoo22 <110867987+Flamingoo22@users.noreply.github.com>
2022-10-26 01:51:53 -0400

    'moreForms'

---
+++ React/functionComponent/assignments/moreform/src/components/MoreForms.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react'
+
+const MoreForms = () => {
+    const [firstName, setFirstName ] = useState('');
+    const [firstNameError, setFirstNameError] = useState('');
+
+    const [lastName, setLastName] = useState('');
+    const [lastNameError, setLastNameError] = useState('');
+
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [confirmPasswordError, setConfirmPasswordError] = useState('');
+
+    const handleError = (e) =>{
+        switch(e.target.id){
+            case 'fn':
+                setFirstName(e.target.value);
+                if(e.target.value.length < 2 && e.target.value !== ''){
+                    setFirstNameError('First Name must be at least 2 characters');
+                }
+                else{
+                    setFirstNameError('');
+                }
+                break;
+            case 'ln':
+                setLastName(e.target.value);
+                if(e.target.value.length < 2 && e.target.value !== ''){
+                    setLastNameError('Last Name must be at least 2 characters');
+                }
+                else{
+                    setLastNameError('');
+                }
+                break;
+            case 'email':
+                setEmail(e.target.value);
+                if(e.target.value.length < 2 && e.target.value !== ''){
+                    setEmailError('Email must be at least 2 characters');
+                }
+                else{
+                    setEmailError('');
+                }
+                break;
+            case 'pw':
+                setPassword(e.target.value);
+                if(e.target.value.length < 8 && e.target.value !== ''){
+                    setPasswordError('Password must be at least 8 characters');
+                }
+                else{
+                    setPasswordError('');
+                }
+                break;
+            case 'cpw':
+                setConfirmPassword(e.target.value);
+                console.log(password)
+                if(e.target.value != password && e.target.value !== ''){
+                    setConfirmPasswordError('Passwords must match');
+                }
+                else{
+                    setConfirmPasswordError('');
+                }
+                break;
+            default:
+                console.log('Why we here?')
+        }
+    }
+
+    return (
+        <form>
+            <div>
+                <label>First Name: </label> 
+                <input type="text" onChange={ handleError } value={firstName} id='fn'/>
+                {
+                    firstNameError ?
+                    <p style={{color:'red'}}>{ firstNameError }</p> :
+                    ''
+                }
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" onChange={ handleError } value={lastName} id='ln'/>
+                {
+                    lastNameError ?
+                    <p style={{color:'red'}}>{ lastNameError }</p> :
+                    ''
+                }
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" onChange={ handleError } value={ email } id='email'/>
+                {
+                    emailError ?
+                    <p style={{color:'red'}}>{ emailError }</p> :
+                    ''
+                }
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="password" onChange={ handleError } value={ password } id='pw'/>
+                {
+                    passwordError ?
+                    <p style={{color:'red'}}>{ passwordError }</p> :
+                    ''
+                }
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input type="password" onChange={ handleError } value={ confirmPassword } id='cpw'/>
+                {
+                    confirmPasswordError ?
+                    <p style={{color:'red'}}>{ confirmPasswordError }</p> :
+                    ''
+                }
+            </div>
+        </form>
+    )
+}
+
+export default MoreForms
---
+++ React/test1/test1/src/App_ConditionalRendering.js
@@ -0,0 +1,10 @@
+import React from "react";
+import UserForm from "./components/ConditionalRendering.jsX";
+
+function App(){
+    return (
+        <UserForm />
+    );
+}
+
+export default App;
